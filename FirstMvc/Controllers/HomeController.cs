4d52a797ce39951cd323a9bad2b7ee5e0c3a5dd2
Harsh Patel <harsh3608@gmail.com>
2023-06-09 18:21:10 +0530

    changes in styles and css, bootstrap classes

--- FirstMvc/Controllers/HomeController.cs
+++ FirstMvc/Controllers/HomeController.cs
@@ -42,5 +42,11 @@ namespace FirstMvc.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+
+        public IActionResult Icons()
+        {
+            return View();
+        }
     }
 }
